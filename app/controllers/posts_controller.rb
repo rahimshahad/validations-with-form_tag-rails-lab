a0845720084a5b91ad1990a876eb7f091e09fc20
rahimshahad <rahim.shahad2@gmail.com>
2021-07-03 14:06:54 -0400

    commit tree e58858b147e9c0fc44a429516365dd4e26dcfc16

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,12 +7,24 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])
   end
 
+  def create
+    @post = Post.new(post_params)
+    if @post.valid?
+      @post.save
+      redirect_to post_path(@post)
+    else 
+      render :new
+    end
+  end
+
   def update
     @post = Post.find(params[:id])
-
     @post.update(post_params)
-
-    redirect_to post_path(@post)
+    if @post.valid?
+      redirect_to post_path(@post)
+    else
+      render :edit
+    end
   end
 
   private
